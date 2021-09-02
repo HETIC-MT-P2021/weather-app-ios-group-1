bbdf47c64924a54c4d956773efa9de9885f7d896
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2021-09-02 17:53:21 +0200

    ✨ (api): Add data model classes and api service

--- weather-app-ios-group-1/ViewController.swift
+++ weather-app-ios-group-1/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let currentForecast = ApiService().getCurrentForecast(lat: 0, lng: 0)
+        
+        debugPrint(currentForecast ?? "Is nil")
     }
-
-
 }
-
