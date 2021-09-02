0c682e69cf5d7251d47141860b0578526017676d
Jacques Maarek <jacques.maarek@hetic.net>
2021-09-02 15:56:11 +0200

    Fix: removed unused files.

--- weather-app-ios-group-1/ViewController.swift
+++ weather-app-ios-group-1/ViewController.swift
@@ -10,29 +10,12 @@ import CoreLocation
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet var table: UITableView!
-
-    var models = [Weather]()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Daily Cell
-        table.register(DailyTableViewCell.nib(), forCellReuseIdentifier: DailyTableViewCell.identifier)
-
-        // Weather Cell
-        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
-
-        table.delegate = self
-        table.dataSource = self
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        LocationManagerService().setupLocation()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return models.count
+        return 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
